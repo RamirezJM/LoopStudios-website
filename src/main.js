3c923bc5d3d357a0339369ea7a685e8e1c9fd1d9
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-08-18 21:55:01 -0400

    feat: add function to mobile menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const buttonMenu = document.querySelector('.btn-hamburguer')
+const navMenu = document.querySelector('.nav-menu')
+const iconHamburguer = document.querySelector('.icon-hamburguer')
+
+buttonMenu.addEventListener('click', () => {
+  if (navMenu.classList.contains('hidden')) {
+    navMenu.classList.remove('hidden')
+    navMenu.classList.add('flex')
+    iconHamburguer.setAttribute('src', 'src/assets/images/icon-close.svg')
+  } else {
+    navMenu.classList.add('hidden')
+    navMenu.classList.remove('flex')
+    iconHamburguer.setAttribute('src', 'src/assets/images/icon-hamburger.svg')
+  }
+
+}) 
